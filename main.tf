0c2bfb4aad17f700899a5dea664a466bb7e24f48
rajendraprasad9 <prasad.gummadavelli@gmail.com>
2020-09-18 11:38:22 +0530

    subnet id

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "myInstance" {
   ami           = "ami-06ce3edf0cff21f07"
   key_name      = "keypairname"
   instance_type = "t2.micro"
+  subnet_id     =  "subnet_id"
   user_data     = <<-EOF
                   #!/bin/bash
                   sudo su
